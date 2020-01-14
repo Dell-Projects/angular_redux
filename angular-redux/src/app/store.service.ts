3eac948e8698feab7b10713f65dfb0a4f445369d
Luis_Rebelo <Luis_rebelo@Dellteam.com>
2020-01-14 16:16:18 -0300

    Implementing store

---
+++ angular-redux/src/app/store.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable, pipe } from 'rxjs';
+import { scan } from 'rxjs/operators';
+import { omit } from 'lodash';
+
+export class Action {
+  
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreService {
+
+  constructor() { }
+}
